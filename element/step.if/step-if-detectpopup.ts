ee8b2ebb11afc32eab008048be2be002f2e7d8a8
Jason Rizio <jason.rizio@gmail.com>
2020-11-04 12:34:06 +1100

    new step.if examples
    added step.if examples for (1) percentage step example and (2) detect popup window and handle

---
+++ element/step.if/step-if-detectpopup.ts
@@ -0,0 +1,62 @@
+import {
+    step,
+    TestSettings,
+    TestData,
+    Until,
+    By,
+    Browser,
+    MouseButtons,
+    Device,
+    beforeAll,
+  } from "@flood/element";
+  import * as assert from "assert";
+
+export const settings: TestSettings = {
+  loopCount: 1,
+  description: 'Detect Popup window and handle',
+  screenshotOnFailure: true,
+  disableCache: true,
+  clearCache: true,
+  clearCookies: true,
+  actionDelay: 6.5,
+  stepDelay: 6.5,
+}
+
+/**
+ * Detect a popup object and if visible then action
+ * Authored by Jason Rizio (jason@flood.io)
+ * Version: 1.0
+ */
+
+export default () => {
+
+    let popupVisible
+
+	step('Popup Example: Home', async (browser) => {
+
+		//Navigate to the SAP Fiori Demo Application
+		await browser.visit('https://wordpress.loadtest.io/sample-page/', {
+			waitUntil: 'load',
+			timeout: 30000,
+		})
+
+        try 
+        {
+            //lets check to see if the modal is displayed
+            popupVisible = await (await browser.findElement(By.css('#sgpb-popup-dialog-main-div-wrapper > div > imgz'))).isDisplayed()
+        }
+        catch { }
+
+    })    
+   
+    step.if(
+		() => popupVisible == true,
+		'Popup Example: Detect and Handle',
+		async browser => {
+			console.log('popupVisible = ' + popupVisible)
+			console.log('Lets handle the popup in this step here.')
+		},
+    )
+
+}
+
---
+++ element/step.if/step-if-percentage.ts
@@ -0,0 +1,66 @@
+import {
+    step,
+    TestSettings,
+    TestData,
+    Until,
+    By,
+    Browser,
+    MouseButtons,
+    Device,
+    beforeAll,
+  } from "@flood/element";
+  import * as assert from "assert";
+
+export const settings: TestSettings = {
+  loopCount: 1,
+  description: 'Unique Test Data',
+  screenshotOnFailure: true,
+  disableCache: true,
+  clearCache: true,
+  clearCookies: true,
+  actionDelay: 2.5,
+  stepDelay: 2.5,
+}
+
+/**
+ * Perecentage Step.if example
+ * Authored by Jason Rizio (jason@flood.io)
+ * Version: 1.0
+ */
+
+export default () => {
+
+	let percentage = 0
+	beforeAll(async () => {
+		percentage = Math.floor(Math.random() * 99 + 1)
+    })
+    
+    step.if(
+		() => percentage < 21,
+		'20Percent',
+		async browser => {
+			console.log('percentage = ' + percentage)
+			console.log('Do this action 20% of the time when percentage is 1-20.')
+		},
+    )
+
+    step.if(
+		() => percentage > 20 && percentage < 51,
+		'20to51Percent',
+		async browser => {
+			console.log('percentage = ' + percentage)
+			console.log('Do this action 30% of the time when percentage is 21-50.')
+		},
+    )
+
+    step.if(
+		() => percentage > 50,
+		'50to100Percent',
+		async browser => {
+			console.log('percentage = ' + percentage)
+			console.log('Do this action 50% of the time when percentage is 50-100.')
+		},
+	)
+
+}
+
